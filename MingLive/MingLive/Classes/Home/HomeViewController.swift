f85cb764d73c3632d6cd4f94341d993a606620d2
刘汝明 <305255147@qq.com>
2016-12-06 14:22:21 +0800

    在navigation修改状态栏

---
+++ MingLive/MingLive/Classes/Home/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  MingLive
+//
+//  Created by MaxM on 2016/12/5.
+//  Copyright © 2016年 MaxM. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        
+        return.lightContent
+    }
+}
